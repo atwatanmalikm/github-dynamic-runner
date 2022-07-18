636980e2f30bcdeda014d9b765dd9522e5e550cb
atwatanmalikm <atwatanmalikm@gmail.com>
2022-07-18 16:47:55 +0700

    update worker-setup

--- worker-setup.sh
+++ worker-setup.sh
@@ -7,7 +7,7 @@ CUR_DIR=$(pwd)
 DIR_ID=$(date +%N)
 
 mkdir -p gha-ctrl-${DIR_ID}
-tar -xf arms/actions-runner-linux-x64-2.293.0.tar.gz -C gha-ctrl-${DIR_ID}
+tar -xf arms/actions-runner-linux.tar.gz -C gha-ctrl-${DIR_ID}
 cd gha-ctrl-${DIR_ID}
 
 #curl --silent -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${AUTH_TOKEN} "  https://api.github.com/repos/$GITHUB_REPOSITORY/actions/runners/registration-token | jq -r .token > runner_token
@@ -61,7 +61,7 @@ export LABEL=\${1}
 export LABELID=\${2}
 
 mkdir -p ~/\${LABELID}
-tar -xf actions-runner-linux-x64-2.293.0.tar.gz -C ~/\${LABELID}
+tar -xf actions-runner-linux.tar.gz -C ~/\${LABELID}
 cd ~/\${LABELID}
 
 curl --silent -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${AUTH_TOKEN} "  https://api.github.com/repos/$GITHUB_REPOSITORY/actions/runners/registration-token | jq -r .token > runner_token
@@ -80,8 +80,8 @@ sleep 30
 while :
 do	
 	sleep 90
-	BR=\$(hostname | cut -d- -f3)
-        ID=\$(hostname | cut -d- -f4)
+	BR=${GITHUB_REF_NAME}
+        ID=${GITHUB_SHA::7}
 	proc=\$(pgrep Runner.Worker | wc -l)
 	while [[ \$proc -ge 1 ]]
 	do
@@ -121,7 +121,7 @@ WantedBy=multi-user.target
 EOF
 
 gcloud compute instances describe gha-worker-${GITHUB_REF_NAME}-${GITHUB_SHA::7} --zone asia-southeast2-a --format=json | jq -r '.networkInterfaces | .[].networkIP' > ip
-scp -o "StrictHostKeyChecking=no" label label_id worker.sh worker-exec.sh worker-rm-exec.sh gha worker-monitor.service  worker-monitor.sh gha@$(cat ip):~/
+scp -o "StrictHostKeyChecking=no" actions-runner-linux.tar.gz label label_id worker.sh worker-exec.sh worker-rm-exec.sh gha worker-monitor.service  worker-monitor.sh gha@$(cat ip):~/
 
 ### Progress
 touch PROGRESS
